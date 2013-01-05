be4b2a09e1cef7f76880ff0b74e4979aba16f112
Steve Kondik <shade@chemlab.org>
2013-01-04 17:28:00 -0800

    hwcomposer: Disable MDP_BACKEND_COMPOSITION for video
     * This can cause the dreaded "pipe still staged" black screen of death.
     * Remove the flag when video is playing.
    
    Change-Id: Ia187c6232e40abe569edef280226340bf32911c6

--- libhwcomposer/hwc_mdpcomp.cpp
+++ libhwcomposer/hwc_mdpcomp.cpp
@@ -190,8 +190,8 @@ int MDPComp::prepare(hwc_context_t *ctx, hwc_layer_1_t *layer,
 
         if(isYuvBuffer(hnd))
             setVidInfo(layer, mdpFlags);
-
-        ovutils::setMdpFlags(mdpFlags,ovutils::OV_MDP_BACKEND_COMPOSITION);
+        else
+            ovutils::setMdpFlags(mdpFlags,ovutils::OV_MDP_BACKEND_COMPOSITION);
 
         if(layer->blending == HWC_BLENDING_PREMULT) {
             ovutils::setMdpFlags(mdpFlags,
